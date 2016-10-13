17ba7ddf5800b526ee5db7ddada0174f07125ed8
scauos <645670644@qq.com>
2016-10-13 22:52:08 +0800

    开始游戏，转场的设定，编辑Scene布局

--- Mr.Pig/ViewController.swift
+++ Mr.Pig/ViewController.swift
@@ -7,19 +7,91 @@
 //
 
 import UIKit
+import SceneKit
+import SpriteKit
 
 class ViewController: UIViewController {
+    
+    let game = GameHelper.sharedInstance
+    var gameScene: SCNScene!
+    var splashScene: SCNScene!
+    var scnView: SCNView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupScenes()
+        setupNodes()
+        setupActions()
+        setupTraffic()
+        setupGestures()
+        setupSounds()
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupScenes() {
+        scnView = SCNView(frame: self.view.frame)
+        self.view.addSubview(scnView)
+        
+        gameScene = SCNScene(named: "/MrPig.scnassets/GameScene.scn")
+        splashScene = SCNScene(named: "/MrPig.scnassets/SplashScene.scn")
+        
+        scnView.scene = splashScene
     }
-
-
+    
+    func setupNodes() {
+        
+    }
+    
+    func setupActions() {
+        
+    }
+    
+    func setupTraffic() {
+        
+    }
+    
+    func setupGestures() {
+        
+    }
+    func setupSounds() {
+        
+    }
+    func startGame() {
+        splashScene.isPaused = true
+        
+        let transition = SKTransition.doorsOpenVertical(withDuration: 1.0)
+        scnView.present(gameScene, with: transition, incomingPointOfView: nil, completionHandler: {
+            self.game.state = .playing
+            self.setupSounds()
+            self.gameScene.isPaused = false
+        })
+    }
+    func stopGame() {
+        game.state = .gameOver
+        game.reset()
+    }
+    func startSplash() {
+        gameScene.isPaused = true
+        
+        let transition = SKTransition.doorsOpenVertical(withDuration: 1.0)
+        scnView.present(splashScene, with: transition, incomingPointOfView: nil) { 
+            self.game.state = .tapToPlay
+            self.setupSounds()
+            self.splashScene.isPaused = false
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if game.state == .tapToPlay {
+            startGame()
+        }
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    override var prefersStatusBarHidden: Bool { return true }
+    override var shouldAutorotate: Bool { return false }
+    
 }
 
