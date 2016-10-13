8e56d3b2abeca96b0381d90400f11abe3fad9748
scauos <645670644@qq.com>
2016-10-13 21:08:03 +0800

    Initial Commit

---
+++ Mr.Pig/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mr.Pig
+//
+//  Created by Hao Dong on 13/10/2016.
+//  Copyright © 2016 Hao Dong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
